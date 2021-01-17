568b89ac478c41e4a347f218506ad8aa0c0aefc5
MARO DE MELO <marodemelo10@gmail.com>
2021-01-17 13:20:13 -0300

    Adicionar arquivos de projeto.

---
+++ Lanches/Models/ErrorViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lanches.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
+
+       
+   
+
+
